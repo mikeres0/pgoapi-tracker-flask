f4a445fd3019bae692701e2ab7ecc83567670808
Mike <mike.resoli@gmail.com>
2016-07-28 16:02:37 +0100

    added flask-login for use at later date

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,13 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+from flask.ext.login import LoginManager, UserMixin, login_required
 
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 @app.route('/')
 def index():
