1a6e78b41a4f13440fef1b2af2a834d80f9d3141
Mike Resoli <mike.resoli@gmail.com>
2016-07-29 10:12:53 +0000

    transfer to pc

--- flask_app.py
+++ flask_app.py
@@ -1,15 +1,19 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, render_template, Response, request
-from flask.ext.login import LoginManager, UserMixin, login_required
+#from flask.ext.login import LoginManager, UserMixin, login_required
 
 
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    return render_template('index.html')
+def sendmessage():
+    email = request.form['email']
+    flash('Thanks for the message ' + email)
     return render_template('index.html')
 
 @app.route('/login')
@@ -17,11 +21,4 @@ def index():
 def underconstruction():
     return render_template('under-construction.html')
 
-@app.route('/contact-submit', methods=['GET'])
-def sendmessage():
-	#email = request.form['email']
-	#reason = requst.form['reason']
-	#message = request.form['message']
-	
-	#if request.method == 'GET':
-	return render_template('index.html')
+
