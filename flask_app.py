0e0936e025abe235c2e30460095ed24607299cfa
Mike Resoli <mike.resoli@gmail.com>
2016-07-29 14:47:09 +0000

    started oauth2 facebook login

--- flask_app.py
+++ flask_app.py
@@ -1,24 +1,73 @@
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template, Response, request
-#from flask.ext.login import LoginManager, UserMixin, login_required
-
+from flask import Flask, render_template, Response, request, flash, json, make_response, session
+from authomatic.adapters import WerkzeugAdapter
+from authomatic import Authomatic
+import requests
 
 app = Flask(__name__)
-#login_manager = LoginManager()
-#login_manager.init_app(app)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.config['OAUTH_CREDENTIALS'] = {
+  'fb': {
+      'class_': oauth2.Facebook,
+      'consumer_key': '293720280981026',
+      'consumer_secret': '8ca2c045bfef4952870eb902a20d4862',
+      'scope': ['user_about_me', 'email', 'read_stream']
+  }
+}
 
+authomatic = Authomatic(app.config['OAUTH_CREDENTIALS'], app.secret_key, report_errors=False)
+
+##### ROUTES
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-def sendmessage():
-    email = request.form['email']
-    flash('Thanks for the message ' + email)
-    return render_template('index.html', email=email)
+    if request.method == 'POST':
+        email = request.form['email']
+        reason = request.form['reason']
+        message = request.form['message']
+        hook = "https://hooks.slack.com/services/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"
+        headers = {'content-type': 'application/json'}
+        payload = {"attachments":[
+                {
+                   "fallback":"new pgoapi-tracker-flask message!",
+                   "pretext":"new pgoapi-tracker-flask message!",
+                   "color":"#28d7e5",
+                   "fields":[
+                      {
+                         "title":"from - "  + email,
+                         "value":"reason - " + reason + "\n\nmessage - " + message,
+                         "short": False
+                      }
+                   ]
+                }
+             ]
+          }
+        r = requests.post(hook, data=json.dumps(payload), headers=headers)
+        if r.status_code == 200:
+          return render_template('index.html', email = email)
+        else:
+          return render_template('index.html', error = r.reason)
+    else:
+        return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+    flash('post')
+  else:
+    return render_template('login.html')
+
 @app.route('/signup')
 def underconstruction():
-    return render_template('under-construction.html')
+   return render_template('index.html', underconst = 'Currently under construction!')
 
-
+@app.route('/login/<provider_name>/', methods=['GET', 'POST'])
+def login(provider_name):
+    response = make_response()
+    result = authomatic.login(WerkzeugAdapter(request, response), provider_name)
+    
+    if result:
+      if result.user:
+        result.user.update()
+      return render_template('login.html', result=result)
+    return response
